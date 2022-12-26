cf57a464b9bb0eede2db8be57725faf6fb7b4a2e
Miguel Klarian <103702076+maklarian@users.noreply.github.com>
2022-12-25 21:11:35 -0300

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -12,8 +12,8 @@ button = st.button("Conectarse...")
 
 if button:
   
-        #conexion = mysql.connector.connect(user='gpt3',password='makc2510', host= '34.176.80.44',database='mygpt3', port='3306')
-        conexion = mysql.connector.connect(user='root',password='makc2510', host= '127.0.0.1',database='gpt3', port='3306')
+        conexion = mysql.connector.connect(user='gpt3',password='makc2510', host= '34.176.80.44',database='mygpt3')
+        #conexion = mysql.connector.connect(user='root',password='makc2510', host= '127.0.0.1',database='gpt3', port='3306')
                 
         
         if conexion.is_connected():
